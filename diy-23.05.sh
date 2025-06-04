61cf462bb031e7db62dbb69bf1c828c6a2fbed8b
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2025-06-04 11:39:00 +0800

    Update diy-23.05.sh

--- diy-23.05.sh
+++ diy-23.05.sh
@@ -155,8 +155,8 @@ clone_all() {
 # 设置编译源码与分支
 REPO_URL="https://github.com/immortalwrt/immortalwrt"
 echo "REPO_URL=$REPO_URL" >>$GITHUB_ENV
-REPO_BRANCH="openwrt-23.05"
-#REPO_BRANCH="openwrt-24.10"
+#REPO_BRANCH="openwrt-23.05"
+REPO_BRANCH="openwrt-24.10"
 echo "REPO_BRANCH=$REPO_BRANCH" >>$GITHUB_ENV
 
 # 开始拉取编译源码
